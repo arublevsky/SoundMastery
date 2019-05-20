13fe7280858756c2ac85462329eb8c25827dad5f
Aleksey Rublevsky <sweexxik@gmail.com>
2019-05-20 22:33:38 +0300

    Added ESLint

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const title = 'My M2222inimal React Webpack Babel Setup';
 
 ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
+    <div>{title}</div>,
+    document.getElementById('app'),
 );
 
 module.hot.accept();
