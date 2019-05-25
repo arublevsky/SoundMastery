8b31d840710fc0b74868baa225f30c63e0295482
Aleksey Rublevsky <a.rublevsky@evision-software.com>
2019-05-25 20:26:22 +0300

    Initial server API with bearer authorization

---
+++ server/SoundMastery/SoundMastery.Api/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoundMastery.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var currentUser = HttpContext.User;
+            if (currentUser.HasClaim(c => c.Type == "DateOfJoining"))
+            {
+                DateTime date = DateTime.Parse(currentUser.Claims.FirstOrDefault(c => c.Type == "DateOfJoining").Value);
+                Console.WriteLine(date); // claims are accessible via current user context.
+            }
+
+            return new[] { "value1", "value2", "value3", "value4", "value5" };
+        }
+    }
+}
